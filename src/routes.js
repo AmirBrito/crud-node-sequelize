da5e63670ccfe7a636703db7e217903a2fcb190d
amirb <amir.sbrito@gmail.com>
2022-10-16 16:25:59 -0300

    feat: add route post create user

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = express.Router();
+const UserController = require('../src/controllers/UserController');
 
 routes.get('/', (req, res) => {
   return res.json({ hello: 'World'});
 })
+
+// User routes
+routes.post('/users', UserController.store);
 
 module.exports = routes;
