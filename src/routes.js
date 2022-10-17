9c0e5fb00d3e0f4cbae3e8d815855cd5d171bb7a
amirb <amir.sbrito@gmail.com>
2022-10-16 22:19:07 -0300

    feat: add product routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const routes = express.Router();
 const UserController = require('../src/controllers/UserController');
+const ProductController = require('../src/controllers/ProductController');
 
 routes.get('/', (req, res) => {
   return res.json({ hello: 'World'});
 })
 
 // User routes
-routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+
+// Product routes
+routes.get('/products', ProductController.index);
+routes.post('/products', ProductController.store);
 
 module.exports = routes;
