9bc2a8396bd099485a2980d5c9aa768e49434818
amirb <amir.sbrito@gmail.com>
2022-10-16 22:19:59 -0300

    feat: add product model

---
+++ src/models/Product.js
@@ -0,0 +1,17 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Product extends Model {
+  static init(sequelize) {
+    super.init({
+      name: DataTypes.STRING,
+    }, {
+      sequelize
+    })
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+  }
+}
+
+module.exports = Product;
