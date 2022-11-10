ce54406afa9fcb8cb3ae91f253c4160eee15ce70
Amir Brito <40916729+AmirBrito@users.noreply.github.com>
2022-11-10 19:34:52 -0300

    feat: add association

--- src/models/Product.js
+++ src/models/Product.js
@@ -8,6 +8,9 @@ class Product extends Model {
       sequelize
     })
   }
+    static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+  }
 }
 
 module.exports = Product;
